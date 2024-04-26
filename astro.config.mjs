d536f13b720ef101ac1a67de1222b91691a08937
Bulu Kucing <bulukucing323@gmail.com>
2024-04-26 14:34:44 +0700

    +ADD: consts variables and metadata components
    ... fix minor style

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://abdulqirom.netlify.app',
   integrations: [tailwind()],
 })
---
+++ src/consts.ts
@@ -0,0 +1,12 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Bulu Kucing'
+export const SITE_DESCRIPTION = 'Bulu Kucing personal webpage'
+
+export const SOCIAL_LINK = {
+  facebook: 'https://facebook.com/abdulqirom.323',
+  twitter: 'https://twitter.com/bulukucing323',
+  github: 'https://github.com/bulukucing',
+  discord: 'https://discordapp.com/channels/@me/adol_bk/',
+}
